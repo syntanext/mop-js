41bb03d222689e1c5e0d0bfef7d9bf1de4359729
hazeezet <azeezibrahim728@gmail.com>
2022-02-05 12:29:04 +0100

    V1.0.0

---
+++ README.md
@@ -0,0 +1,98 @@
+# mop-js
+create a long live connection between your application and mysql in a javascript server ( NODEJS ). this is not mysql pool.
+
+this package helps you to manage auto reconnection.
+
+you still have the complete features of mysql in nodejs except for mysql pool
+
+## INSTALLATION
+> npm i @bringittocode/mop-js
+
+## FEATURES
+
+* Auto reconnection if connection timeout or an error occured
+* You still have all the method of [MYSQL](https://www.npmjs.com/package/mysql) except for pool.
+* You have an event to listen on if connection failed or successful
+
+## USAGE
+
+### import the module
+```js
+import mop from "@bringittocode/mop-js";
+
+//OR
+const mop = require('@bringittocode/mop-js');
+```
+
+### Register the module
+```js
+mop.register({
+    DATABASE_INFO: {
+        host: "",
+        user: "",
+        password: "",
+        database: "",
+    },
+    WAIT_TIME: 2000,
+});
+```
+
+> DATABASE_INFO => your database detailes.
+
+> WAIT_TIME => how many miliseconds to wait before reconnecting if there is any disconnection.. DEFAULT 5 seconds
+
+After registering you can now import mysql instance in all of your application
+```js
+
+import {DB, DB_INFO} from "@bringittocode/mop-js";
+
+```
+
+what you have to do is to focus on your query as you would if no package is used
+
+learn more => https://www.npmjs.com/package/mysql;
+
+```js
+    const query = "SELECT * FROM your_table where ?";
+    DB.query(query, ["user"], function (err, result) {
+        try {
+            if (err) throw err;
+            //manage result
+        }
+        catch(err)
+        {
+            //manage error
+        }
+    });
+```
+> NOTE :: do not disconnect after you finish... your connection is meant to stay alive so you can query your database at anytime
+
+## Event
+
+event must be inside fastify ready function to avoid error
+```js
+    // listen for connection and reconnection
+    // status => boolean
+    DB_INFO.on("connect",(status)=>{
+        console.log('database connected');
+    });
+
+    // listen for reconnection error
+    // code => error code
+    // message => error message
+    DB_INFO.on("reconnect_error", (code,message) => {
+        console.log(code, message);
+    });
+
+    // listen for disconnection ...this can be for many reason
+    DB_INFO.on("disconnect", (code) => {
+        console.log(code);
+    });
+```
+
+That all you need.
+We are finding who can add typescript to this package
+
+feel free to pull request and contribute....
+
+any issue feel free as well.
---
+++ index.js
@@ -0,0 +1,87 @@
+"use strict"
+
+import events from "events"
+import mysql from "mysql";
+
+/**
+ * connect to MYSQL Database..
+ * 
+ * this is a long live connection and not a connection pool
+ */
+class mop{
+
+    static DB = '';
+    static DB_INFO = '';
+
+    static register (options) {
+        const {
+            DATABASE_INFO: DATABASE_INFO = {
+                host: 'localhost',
+                user: '',
+                password: '',
+                database: ''
+            }
+        } = options;
+        const {
+            WAIT_TIME: WAIT_TIME = 5000
+        } = options;
+
+        // test connection 
+        const connection = mysql.createConnection(DATABASE_INFO);
+
+        const emitter = new events.EventEmitter();
+        // store test connection
+        this.DB = connection;
+        this.DB_INFO = emitter;
+
+        const CONNECT = async function () {
+            try {
+                // new connection
+                const new_connection = await new Promise(async (resolve, reject) => {
+    
+                    if (connection.state === "disconnected") {
+    
+                        const connection = mysql.createConnection(DATABASE_INFO);
+    
+                        connection.connect(function (error) {
+                            if (error) {
+                                const code = error.code;
+                                const message = error.message;
+                                emitter.emit("reconnect_error", code, message)
+                                setTimeout(CONNECT, WAIT_TIME);
+                            } else {
+                                resolve(connection);
+                            }
+    
+                        });
+                    } else {
+                        reject('already connected');
+                    }
+    
+                });
+    
+                this.DB = new_connection;
+    
+                emitter.emit("connect", true)
+    
+                this.DB.on('error', function (error) {
+                    const code = error.code;
+                    emitter.emit("disconnect", code)
+                    mop.DB.destroy()
+                    CONNECT();
+                });
+            } catch (e) {
+                emitter.emit("connect", true)
+                this.DB.on('error', function (error) {
+                    const code = error.code;
+                    emitter.emit("disconnect", code);
+                    mop.DB.destroy();
+                    CONNECT();
+                });
+            }
+        }
+        CONNECT()
+    }
+}
+
+export default mop;
